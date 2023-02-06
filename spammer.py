bad9a3772cdeab2b58b2a9c786a7109d81119e6a
miksme <miks_mednis@inbox.lv>
2023-02-06 02:45:28 +0200

    fix: now spams empty commits

--- spammer.py
+++ spammer.py
@@ -1,9 +1,10 @@
 import subprocess
 import argparse
-
+import os
 def git(*args):
   return subprocess.check_call(['git'] + list(args))
-
+def switchBranch(name):
+  os.system(f"git checkout {name} 2>/dev/null || git checkout -b {name}") # Switch to branch and create it if it does not exist
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-b', '--branch', type=str, default='spam', help='Branch to spam on')
@@ -16,12 +17,9 @@ startingBranch = stdout.decode("utf-8").splitlines()[0]
 returnToB = False
 if(startingBranch != args['branch']):
   returnToB = True
-  git('switch', '-c', args['branch'])
+  switchBranch(args['branch'])
 for i in range(args['commits']):
-    open('./spam/file.txt', 'a').write('1')
-    git('add','./spam/file.txt')
-    git('commit','-m', 'feat: new spam entry')
-  
+    git('commit','--allow-empty','-m', 'feat: new spam entry')
 git('push', '-u', 'origin', args['branch'])
 if returnToB:
   git('checkout', startingBranch)
