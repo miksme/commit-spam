90b36e143ca13cc9ba227924432bf317aec2ad2a
miksme <miks_mednis@inbox.lv>
2023-02-06 02:48:02 +0200

    feat: ability to change working dir

--- spammer.py
+++ spammer.py
@@ -9,8 +9,11 @@ def switchBranch(name):
 parser = argparse.ArgumentParser()
 parser.add_argument('-b', '--branch', type=str, default='spam', help='Branch to spam on')
 parser.add_argument('-c', '--commits', type=int, default=100, help='Number of commits to make')
+parser.add_argument('-d', '--dir', type=str, help='Working directory')
 args = vars(parser.parse_args())
-
+if args['dir']:
+  os.chdir(args['dir'])
+print(f"Current working dir changed is {os.getcwd()}")
 p = subprocess.Popen(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 stdout, stderr = p.communicate()
 startingBranch = stdout.decode("utf-8").splitlines()[0]
